74b9892b89c3eacb1703e6bddf20ca9866d9f1f9
Simon Weil <weils@catlin.edu>
2023-05-30 17:33:32 -0700

    feat: functional websocket server and routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<center>
+    <div>
+        <a href="/host">
+            <button>Host</button>
+        </a>
+        <br>
+        <a href="/player">
+            <button>Player</button>
+        </a>
+    </div>
+</center>
+
