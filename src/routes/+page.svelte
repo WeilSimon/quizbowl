fb6c1b067509f8ecd0005744177ee4b9c98fd2f4
Simon Weil <weils@catlin.edu>
2023-06-01 09:08:44 -0700

    feat: in progress things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,38 @@
+<div class="outer">
+	<center>
+		<h1>Select a Role</h1>
+		<div class="vertcenter">
+			<a href="/host">
+				<button class="btn btn-secondary">Host</button>
+			</a>
+			<br />
+			<a href="/player">
+				<button class="btn btn-secondary">Player</button>
+			</a>
+		</div>
+	</center>
+</div>
 
-<center>
-    <div>
-        <a href="/host">
-            <button>Host</button>
-        </a>
-        <br>
-        <a href="/player">
-            <button>Player</button>
-        </a>
-    </div>
-</center>
-
+<style>
+	.outer {
+        padding:20px;
+        position: absolute;
+        top:0;
+        bottom:0;
+        left:0;
+        right:0;
+        margin:0;
+        width:100vw;
+		background-image: linear-gradient(to left, hsl(0, 0%, 90%), hsl(0, 0%, 70%));
+	}
+	
+	h1 {
+		margin-bottom: 100px;
+	}
+	button {
+		width: 50vw;
+		font-size: 18pt;
+		padding: 20px;
+		margin-bottom: 30px;
+	}
+</style>
