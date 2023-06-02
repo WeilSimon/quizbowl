9224509f90cc2314af421156e71e22de649b5cb1
WeilSimon <weils@catlin.edu>
2023-06-01 20:47:25 -0700

    feat: points, buzz, buzz list, locking, bootsrap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,11 @@
 		<h1>Select a Role</h1>
 		<div class="vertcenter">
 			<a href="/host">
-				<button class="btn btn-secondary">Host</button>
+				<button class="btn btn-primary">Host</button>
 			</a>
 			<br />
 			<a href="/player">
-				<button class="btn btn-secondary">Player</button>
+				<button class="btn btn-primary">Player</button>
 			</a>
 		</div>
 	</center>
@@ -22,8 +22,7 @@
         left:0;
         right:0;
         margin:0;
-        width:100vw;
-		background-image: linear-gradient(to left, hsl(0, 0%, 90%), hsl(0, 0%, 70%));
+        width:100vw; 
 	}
 	
 	h1 {
